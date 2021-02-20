7db19d2ac71683de62bdbda6accf892bbe5c1c45
Anthony Faulds <afaulds@trinnovations.com>
2021-02-19 21:38:37 -0800

    Update noise correction to class to abstract parallelism

---
+++ NoiseCorrection.py
@@ -0,0 +1,74 @@
+import math
+from multiprocessing import Process, Manager
+import numpy as np
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import KFold
+
+
+class NoiseCorrection:
+
+    def __init__(self, X, y):
+        self.M = 5 # Number of scores per item.
+        self.K = 10 # Number of folds.
+        self.X = X
+        self.y = y
+        self.r = None
+        self.theta = 0
+        # Thread safe objects.
+        manager = Manager()
+        self.Bc = manager.list(np.zeros(len(y)))
+        self.Be = manager.list(np.zeros(len(y)))
+
+    def calculate_noise(self):
+        jobs = []
+        # Calculate Be and Bc
+        for m in range(self.M):
+            kf = KFold(n_splits=self.K)
+            for train_index, test_index in kf.split(self.X):
+                print('.', end='', flush=True)
+                p = Process(target=self.train_and_eval, args=(train_index, test_index))
+                p.start()
+                jobs.append(p)
+        print('All started.', flush=True)
+
+        for job in jobs:
+            job.join()
+            print('*', end='', flush=True)
+        print('All complete.', flush=True)
+
+        # Calculate Be_total
+        Be_total = np.sum(self.Be)
+
+        # Calculate r (noise) and theta (threshold)
+        self.r = np.zeros(len(self.y))
+        self.theta = 0
+        for i in range(len(self.y)):
+            self.r[i] = self.Bc[i] + (1 - self.Be[i] / Be_total)
+            if self.Bc[i] >= self.M / 2:
+                self.theta += 1
+
+    def get_noise_index(self, fracion=1.0):
+        all_index = range(len(self.y))
+        sorted_index = [x for _,x in sorted(zip(self.r, all_index), reverse=True)]
+        return sorted_index[0:int(self.theta * fracion)]
+
+    def train_and_eval(self, train_index, test_index):
+        X_train = self.X[train_index]
+        y_train = self.y[train_index]
+        X_test = self.X[test_index]
+        y_test = self.y[test_index]
+        clf = GradientBoostingClassifier(
+            n_estimators=10,
+            learning_rate=0.8,
+            max_depth=5
+        ).fit(X_train, y_train)
+        y_scores = clf.predict(X_test)
+        y_prob = clf.predict_proba(X_test)
+        for p in range(len(test_index)):
+            i = test_index[p]
+            c = y_prob[p][0]
+            d = y_prob[p][1]
+            if c > 0.0 and d > 0.0:
+                self.Be[i] -= c * math.log2(c) + d * math.log2(d)
+            if self.y[i] != y_scores[p]:
+                self.Bc[i] += 1
--- train.py
+++ train.py
@@ -1,30 +1,34 @@
 import pickle
-from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 import numpy as np
 import matplotlib.pyplot as plt
 import random
-from sklearn.model_selection import KFold
-import math
+from NoiseCorrection import NoiseCorrection
 
 
 def main():
-    x = []
-    y = []
-    for i in range(10):
-        score_orig, score_nonoise = test_noise_removal()
-        x.append(score_orig)
-        y.append(score_nonoise)
+    a = []
+    b = []
+    c = []
+    d = []
+    for i in range(30):
+        score_perfect, score_dirty, score_nonoise, score_clean = test_noise_removal()
+        a.append(score_perfect)
+        b.append(score_dirty)
+        c.append(score_nonoise)
+        d.append(score_clean)
+    print("Average perfect  {:.2f} {:.2f}".format(np.mean(a), np.std(a)))
+    print("Average dirty    {:.2f} {:.2f}".format(np.mean(b), np.std(b)))
+    print("Average no noise {:.2f} {:.2f}".format(np.mean(c), np.std(c)))
+    print("Average clean    {:.2f} {:.2f}".format(np.mean(d), np.std(d)))
 
-    print("Average base score {}".format(np.mean(x), ))
-    print("Average no noise score {}".format(np.mean(y)))
 
 def test_noise_removal():
     print("-------------------------------------")
     # Read from standardized file.
-    with open('data/Simple2.pkl', 'rb') as infile:
+    with open('data/Simple.pkl', 'rb') as infile:
         data = pickle.loads(infile.read())
 
     # Create training and test set.
@@ -36,9 +40,17 @@ def test_noise_removal():
 
     plot(X_train, y_train, "output/step0.png")
 
+    # Train and score if features were perfect.
+    clf1 = GradientBoostingClassifier(
+        n_estimators=10,
+        learning_rate=0.8,
+        max_depth=5
+    ).fit(X_train, y_train)
+    score_perfect = clf1.score(X_test, y_test)
+
     # Randomize training set a little to introduce noise.
     changed = np.zeros(len(y_train))
-    randomize(0.25, y_train, changed)
+    randomize(0.2, y_train, changed)
     print("Noise introduced wrong {}".format(np.sum(changed)))
     print("Training size {}".format(len(y_train)))
 
@@ -46,46 +58,11 @@ def test_noise_removal():
     plot(X_train, y_train, "output/step1.png")
 
     # Train model.
-    clf = GradientBoostingClassifier(
-        n_estimators=10,
-        learning_rate=0.8,
-        max_depth=5
-    ).fit(X_train, y_train)
-
-    # Predict and convert to single array.
-    y_scores = clf.predict_proba(X_test)
-    y_predict = np.zeros(len(y_scores))
-    for i in range(len(y_scores)):
-        y_predict[i] = y_scores[i][1]
-
-    # Get accuracy and precision curves.
-    fpr, tpr, thresholds = metrics.roc_curve(y_test, y_predict)
-    plt.plot(thresholds, fpr, 'r-', thresholds, tpr, 'g-')
-    #plt.show()
-
-    # Show score.
-    score_base = clf.score(X_test, y_test)
-    print("Training score {}".format(clf.score(X_train, y_train)))
-    print("Validation score {}".format(clf.score(X_test, y_test)))
-    print("")
-
-    all_index = range(len(y_train))
-    noise = find_noise(X_train, y_train)
-    good_index = np.setdiff1d(all_index, noise)
-    X_train_new = X_train[good_index]
-    y_train_new = y_train[good_index]
-    print("Data removed {}".format(len(noise)))
-    print("Noise removed {}".format(np.sum(changed[noise])))
-    print("Training size {}".format(len(y_train_new)))
-
-    plot(X_train_new, y_train_new, "output/step2.png")
-
-    # Train model.
     clf2 = GradientBoostingClassifier(
         n_estimators=10,
         learning_rate=0.8,
         max_depth=5
-    ).fit(X_train_new, y_train_new)
+    ).fit(X_train, y_train)
 
     # Predict and convert to single array.
     y_scores = clf2.predict_proba(X_test)
@@ -99,12 +76,60 @@ def test_noise_removal():
     #plt.show()
 
     # Show score.
-    score_nonoise = clf2.score(X_test, y_test)
-    print("Training score {}".format(clf2.score(X_train_new, y_train_new)))
+    score_dirty = clf2.score(X_test, y_test)
+    print("Training score {}".format(clf2.score(X_train, y_train)))
     print("Validation score {}".format(clf2.score(X_test, y_test)))
     print("")
 
-    return score_base, score_nonoise
+    # Find noisy elements
+    y_train_cleaned = y_train.copy()
+    nc = NoiseCorrection(X_train, y_train)
+    nc.calculate_noise()
+    noise_set = nc.get_noise_index()
+
+    # Remove noise
+    all_index = range(len(y_train))
+    good_index = np.setdiff1d(all_index, noise_set)
+    X_train_new = X_train[good_index]
+    y_train_new = y_train[good_index]
+    print("Data removed {}".format(len(noise_set)))
+    print("Noise removed {}".format(np.sum(changed[noise_set])))
+    print("Training size {}".format(len(y_train_new)))
+
+    plot(X_train_new, y_train_new, "output/step2.png")
+
+    # Train model.
+    clf3 = GradientBoostingClassifier(
+        n_estimators=10,
+        learning_rate=0.8,
+        max_depth=5
+    ).fit(X_train_new, y_train_new)
+
+    # Predict and convert to single array.
+    y_scores = clf3.predict_proba(X_test)
+    y_predict = np.zeros(len(y_scores))
+    for i in range(len(y_scores)):
+        y_predict[i] = y_scores[i][1]
+
+    # Get accuracy and precision curves.
+    fpr, tpr, thresholds = metrics.roc_curve(y_test, y_predict)
+    plt.plot(thresholds, fpr, 'r-', thresholds, tpr, 'g-')
+    #plt.show()
+
+    # Show score.
+    score_nonoise = clf3.score(X_test, y_test)
+    print("Training score {}".format(clf3.score(X_train_new, y_train_new)))
+    print("Validation score {}".format(clf3.score(X_test, y_test)))
+    print("")
+
+    clf4 = GradientBoostingClassifier(
+        n_estimators=10,
+        learning_rate=0.8,
+        max_depth=5
+    ).fit(X_train, y_train_cleaned)
+    score_clean = clf4.score(X_test, y_test)
+
+    return score_perfect, score_dirty, score_nonoise, score_clean
 
 
 def randomize(percent, y, changed):
@@ -129,48 +154,6 @@ def plot(X, y, file_name):
     plt.plot(x1, y1, 'r^', x2, y2, 'bs')
     plt.savefig(file_name)
     plt.close()
-
-
-def find_noise(X, y):
-    m = 5 # Number of scores per item.
-    k = 10 # Number of folds.
-    y_predicts = np.zeros((len(y), m))
-    Bc = np.zeros(len(y))
-    Be = np.zeros(len(y))
-    all_index = range(len(y))
-    Be_total = 0
-    for j in range(m):
-        kf = KFold(n_splits=k)
-        for train_index, test_index in kf.split(X):
-            X_train, X_test = X[train_index], X[test_index]
-            y_train, y_test = y[train_index], y[test_index]
-
-            clf = GradientBoostingClassifier(
-                n_estimators=10,
-                learning_rate=0.8,
-                max_depth=5
-            ).fit(X_train, y_train)
-            y_scores = clf.predict(X_test)
-            y_prob = clf.predict_proba(X_test)
-
-            for p in range(len(test_index)):
-                i = test_index[p]
-                c = y_prob[p][0]
-                d = y_prob[p][1]
-                if c > 0.0 and d > 0.0:
-                    Be[i] -= c * math.log2(c) + d * math.log2(d)
-                Be_total += Be[i]
-                if y[i] != y_scores[p]:
-                    Bc[i] += 1
-
-    r = np.zeros(len(y))
-    theta = 0
-    for i in range(len(y)):
-        r[i] = Bc[i] + (1 - Be[i] / Be_total)
-        if Bc[i] >= m / 2:
-            theta += 1
-    sorted_index = [x for _,x in sorted(zip(r, all_index), reverse=True)]
-    return sorted_index[0:theta]
 
 
 if __name__ == '__main__':
