ec8ba1ea8a1b37241848a05ccfc5b8b452b9f609
Anthony Faulds <afaulds@trinnovations.com>
2021-04-13 19:23:04 -0700

    Setup noise removal and correction

--- NoiseCorrection.py
+++ NoiseCorrection.py
@@ -18,23 +18,29 @@ class NoiseCorrection:
         manager = Manager()
         self.Bc = manager.list(np.zeros(len(y)))
         self.Be = manager.list(np.zeros(len(y)))
+        self.H = manager.list(np.zeros(len(y)))
+        self.Q = manager.list()
+        for i in range(self.M):
+            self.Q.append(manager.list())
+        self.noise_set = []
 
     def calculate_noise(self):
         jobs = []
-        # Calculate Be and Bc
+        # Run all k-fold models in parallel.
         for m in range(self.M):
             kf = KFold(n_splits=self.K)
             for train_index, test_index in kf.split(self.X):
-                print('.', end='', flush=True)
-                p = Process(target=self.train_and_eval, args=(train_index, test_index))
+                p = Process(target=self.train_and_eval, args=(m, train_index, test_index))
                 p.start()
                 jobs.append(p)
-        print('All started.', flush=True)
+                print(".", end="", flush=True)
+        print("All started.", flush=True)
 
+        # Wait for jobs to complete.
         for job in jobs:
             job.join()
-            print('*', end='', flush=True)
-        print('All complete.', flush=True)
+            print("*", end="", flush=True)
+        print("All complete.", flush=True)
 
         # Calculate Be_total
         Be_total = np.sum(self.Be)
@@ -50,9 +56,46 @@ class NoiseCorrection:
     def get_noise_index(self, fracion=1.0):
         all_index = range(len(self.y))
         sorted_index = [x for _,x in sorted(zip(self.r, all_index), reverse=True)]
-        return sorted_index[0:int(self.theta * fracion)]
+        self.noise_set = sorted_index[0:int(self.theta * fracion)]
+        return self.noise_set
 
-    def train_and_eval(self, train_index, test_index):
+    def get_clean(self):
+        jobs = []
+        for m in range(self.M):
+            p = Process(target=self.train, args=(self.Q[m],))
+            p.start()
+            jobs.append(p)
+            print(".", end="", flush=True)
+        print("All started.", flush=True)
+
+        # Wait for jobs to complete.
+        for job in jobs:
+            job.join()
+            print("*", end="", flush=True)
+        print("All complete.", flush=True)
+        y_new = self.y.copy()
+        for p in range(len(self.noise_set)):
+            i = self.noise_set[p]
+            if self.H[p] > self.M / 2:
+                y_new[i] = 0
+            else:
+                y_new[i] = 1
+        return y_new
+
+    def train(self, train_index):
+        X_train = self.X[train_index]
+        y_train = self.y[train_index]
+        X_test = self.X[self.noise_set]
+        clf = GradientBoostingClassifier(
+            n_estimators=10,
+            learning_rate=0.8,
+            max_depth=5
+        ).fit(X_train, y_train)
+        y_prob = clf.predict_proba(X_test)
+        for i in range(len(self.noise_set)):
+            self.H[i] += y_prob[i][0]
+
+    def train_and_eval(self, m, train_index, test_index):
         X_train = self.X[train_index]
         y_train = self.y[train_index]
         X_test = self.X[test_index]
@@ -72,3 +115,6 @@ class NoiseCorrection:
                 self.Be[i] -= c * math.log2(c) + d * math.log2(d)
             if self.y[i] != y_scores[p]:
                 self.Bc[i] += 1
+            else:
+                self.Q[m].append(i)
+
