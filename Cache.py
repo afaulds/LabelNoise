45811f0b101d8ca777563675329b39a7f91322f2
Anthony Faulds <afaulds@trinnovations.com>
2021-04-18 21:07:24 -0700

    Update Cache.py
    - Update cache with subdirectory.

--- Cache.py
+++ Cache.py
@@ -1,14 +1,13 @@
 import json
 import tempfile
-from os import path
+import os
 import hashlib
 
 
 class Cache:
 
     def process(key, func, *args):
-        clean_key = hashlib.md5(str(key).encode()).hexdigest()
-        file_name = tempfile.gettempdir() + "/" + clean_key + ".tmp"
+        file_name = Cache.__get_file_name(key)
         if Cache.__key_exists(file_name):
             return Cache.__read_file(file_name)
         else:
@@ -16,13 +15,24 @@ class Cache:
             Cache.__write_file(file_name, val)
             return val
 
+    def __get_file_name(key):
+        clean_key = hashlib.md5(str(key).encode()).hexdigest()
+        a = clean_key[0]
+        b = clean_key[1]
+        file_name = "{}/PyCache/{}/{}/{}.json".format(tempfile.gettempdir(), a, b, clean_key)
+        file_name = os.path.abspath(file_name)
+        return file_name
+
     def __key_exists(file_name):
-        return path.exists(file_name)
+        return os.path.exists(file_name)
 
     def __read_file(file_name):
         with open(file_name, "r") as infile:
             return json.loads(infile.read())
 
     def __write_file(file_name, val):
+        dir = os.path.dirname(file_name)
+        if not os.path.exists(dir):
+            os.makedirs(dir)
         with open(file_name, "w") as outfile:
             outfile.write(json.dumps(val))
