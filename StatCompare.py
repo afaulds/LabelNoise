9e36365a076f6ca2bcc5c682f4dfad10542f7663
Anthony Faulds <afaulds@trinnovations.com>
2021-04-19 13:13:35 -0700

    Summary
    - Setup summarize script
    - Add mean to stat compare

--- StatCompare.py
+++ StatCompare.py
@@ -4,6 +4,16 @@ import numpy as np
 
 class StatCompare:
 
+    def mean(stat_a):
+        stat = {}
+        keys = list(stat_a[0].keys())
+        for key in keys:
+            a = []
+            for i in range(len(stat_a)):
+                a.append(stat_a[i][key])
+            stat[key] = np.mean(a)
+        return stat
+
     def diff(stat_a, stat_b):
         stat = {}
         keys = list(stat_a[0].keys())
---
+++ summarize.py
@@ -0,0 +1,61 @@
+import numpy as np
+import os
+from time import time
+import NoiseCorrection_v0 as v0
+import NoiseCorrection_v1 as v1
+import NoiseCorrection_v2 as v2
+import NoiseCorrection_v3 as v3
+import NoiseCorrection_v4 as v4
+from StatCompare import StatCompare
+from Cache import Cache
+from Grid import Grid
+
+
+num_repeat_runs = 20
+input_data_files = [
+    "data/Biodeg.pkl",
+    "data/Ionosphere.pkl",
+    "data/Krvskp.pkl",
+    "data/Mushroom.pkl",
+#    "data/Sick.pkl",
+    "data/Simple.pkl",
+    "data/Simple2.pkl",
+    "data/Spam.pkl",
+    "data/Tictactoe.pkl",
+    "data/Unbalanced.pkl",
+    "data/Vote.pkl",
+]
+noise_types = [
+    v0.NoiseCorrection.get_name(),
+    v1.NoiseCorrection.get_name(),
+    v2.NoiseCorrection.get_name(),
+    v3.NoiseCorrection.get_name(),
+    v4.NoiseCorrection.get_name(),
+]
+
+
+def main():
+    init()
+    start_time = time()
+    grid = Grid()
+    grid.load("test.txt")
+    for file_name in input_data_files:
+        for noise_name in noise_types:
+            scores = []
+            for i in range(num_repeat_runs):
+                key = (i, noise_name, file_name)
+                score = Cache.get(key)
+                scores.append(score)
+            grid.set(file_name, noise_name, StatCompare.mean(scores)['no_noise_100'])
+    grid.save("test.txt")
+    end_time = time()
+    print("Overall time: {}".format(end_time - start_time))
+
+
+def init():
+    if not os.path.exists('output'):
+        os.makedirs('output')
+
+
+if __name__ == "__main__":
+    main()
